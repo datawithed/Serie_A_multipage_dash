e5406fa3825436209916c92a33567ef4c1e2e9eb
Ed Morris <102766519+datawithed@users.noreply.github.com>
2022-07-03 11:20:56 +0100

    Update pass_maps.py

--- pages/pass_maps.py
+++ pages/pass_maps.py
@@ -25,8 +25,8 @@ font = {'fontname':'Tahoma'}
 # Load data
 def load_data():
     # Set wd path
-    rfp_path = r"C:\Users\ed.morris\Documents\Python Scripts\Wyscout"
-    os.chdir(rfp_path)
+    #rfp_path = r"C:\Users\ed.morris\Documents\Python Scripts\Wyscout"
+    #os.chdir(rfp_path)
 
     # Read in json to variable 'data'
     with open('Sample event data.txt') as json_file:
